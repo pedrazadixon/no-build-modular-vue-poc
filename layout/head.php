4f3240ca32aeb70f25a323e069e254ebfcdfcc7f
Dixon Pedraza <pedrazadixon@gmail.com>
2025-11-21 19:06:08 -0500

    feat: Add initial HTML head structure with CSS links and basic tab component styling.

--- layout/head.php
+++ layout/head.php
@@ -8,36 +8,5 @@ define('BASE_URL', 'http://localhost/no-build-modular-vue-poc/');
     <title>POC</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons" rel="stylesheet" type="text/css" />
     <link href="https://cdn.jsdelivr.net/npm/quasar@2.18.6/dist/quasar.prod.css" rel="stylesheet" type="text/css" />
-
-
-    <style>
-        html,
-        body {
-            height: 100%;
-        }
-
-        .tab-content {
-            transition: transform 0.25s ease, opacity 0.25s ease;
-            transform: translate(100%, 2%);
-            opacity: 0;
-            pointer-events: none;
-        }
-
-        .tab-content.active {
-            transform: translate(0, 0);
-            opacity: 1;
-            pointer-events: auto;
-            position: relative !important;
-        }
-
-        .tab-with-close .close-tab-btn {
-            opacity: 0;
-            transition: opacity 0.2s ease;
-        }
-
-        .tab-with-close:hover .close-tab-btn {
-            opacity: 1;
-        }
-    </style>
-
+    <link href="<?php echo BASE_URL ?>assets/css/styles.css" rel="stylesheet" type="text/css" />
 </head>
