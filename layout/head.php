f8511a128fc692ba6e22cd1e5f63996005259c7a
Dixon Pedraza <pedrazadixon@gmail.com>
2026-01-20 18:08:46 -0500

    refactor: append versioning to styles.css link for cache busting

--- layout/head.php
+++ layout/head.php
@@ -25,7 +25,7 @@ if (!defined('APP_VERSION')) {
     <title>POC</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons" rel="stylesheet" type="text/css" />
     <link href="https://cdn.jsdelivr.net/npm/quasar@2.18.6/dist/quasar.prod.css" rel="stylesheet" type="text/css" />
-    <link href="<?php echo BASE_URL ?>assets/css/styles.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo BASE_URL ?>assets/css/styles.css?v=<?php echo APP_VERSION; ?>" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
